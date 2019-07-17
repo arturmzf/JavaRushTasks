b292f4fe3bd850ac365a129386236f350ee4b79e
Artur Muzafarov <art.mzf@gmail.com>
2019-07-18 00:32:54 +0300

    Level 01 - Lection 03 - All Other Tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0110/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0110;
+
+/* 
+На нашем экране — переменная
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        String name = "Arturka";
+
+        System.out.println(name);
+    }
+}
